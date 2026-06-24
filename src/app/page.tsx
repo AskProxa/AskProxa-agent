3218282d1ec1a29c582421902b6ac254ae581399
AskProxa <waqvaga@gmail.com>
2026-06-24 13:02:16 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import VerificationCard from "@/components/VerificationCard";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <VerificationCard />
+    </>
+  );
+}
